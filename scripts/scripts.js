d57f5650dfe82badb1fa789221033a60cec27181
Antonella Testa <antonellamtesta@gmail.com>
2020-05-01 17:55:14 +0100

    First commit

---
+++ scripts/scripts.js
@@ -0,0 +1,59 @@
+console.log('Script file loaded.');
+
+$(document).ready(function () {
+  //To loads google map (save time).
+  $('iframe').show();
+
+  //hamburger menu
+  var toggleElements = function () {
+    $('nav').toggleClass('showMenu');
+  };
+
+  $('.hamburger-menu, .menu li a').on('click', toggleElements);
+  //end hamburger menu
+
+
+  //accordion
+  $('.accordion > div').click(function () {
+    $(this).toggleClass('active');
+    $('.accordion .active').not(this).removeClass('active');
+  });
+  //end accordion
+
+
+  //modal box
+  var modalToggle = function () {
+    $('#modal-privacy-policy').toggleClass('active');
+  }
+  $('#privacy-policy, #modal-privacy-policy i').on('click', modalToggle);
+  //end modal box
+
+
+  //form submit
+  var submitForm = function (event) {
+    event.preventDefault();
+    var data = {
+      name: $(this).find('input[name="fullName"]').val(),
+      email: $(this).find('input[name="email"]').val(),
+      comments: $(this).find('textarea[name="comments"]').val(),
+    }
+
+    if (data.name, data.email, data.comments) {
+      $.ajax({
+        url: '',
+        data: data,
+        method: 'POST',
+        success: toggleSuccess,
+      });
+    }
+  }
+  $('form').on('submit', submitForm);
+  $('#modal-success i').on('click', toggleSuccess);
+
+
+  /* Add or remove class */
+  function toggleSuccess() {
+    $('#modal-success').toggleClass('active');
+  }
+
+});
